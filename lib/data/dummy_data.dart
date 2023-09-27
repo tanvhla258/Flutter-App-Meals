93601d0e0ef2f25d83718507115ee19d24d0d846
tanvhla258 <tanvhla258@gmail.com>
2023-09-27 16:26:42 +0700

    first commit

---
+++ lib/data/dummy_data.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+import 'package:meals/models/category.dart';
+
+const availableCategories = [
+  Category(
+    id: 'c1',
+    title: 'Italian',
+    color: Colors.purple,
+    backgroundImage: 'italian.jpg',
+  ),
+  Category(
+    id: 'c2',
+    title: 'Quick & Easy',
+    color: Colors.red,
+    backgroundImage: 'quick_and_esay.jpg',
+  ),
+  Category(
+    id: 'c3',
+    title: 'Hamburgers',
+    color: Colors.orange,
+    backgroundImage: 'hamburger.jpeg',
+  ),
+  Category(
+    id: 'c4',
+    title: 'German',
+    color: Colors.amber,
+    backgroundImage: 'german.jpg',
+  ),
+  Category(
+    id: 'c5',
+    title: 'Light & Lovely',
+    color: Colors.blue,
+    backgroundImage: 'live_and_lovely.jpg',
+  ),
+  Category(
+    id: 'c6',
+    title: 'Exotic',
+    color: Colors.green,
+    backgroundImage: 'exotic.jpg',
+  ),
+  Category(
+    id: 'c7',
+    title: 'Breakfast',
+    color: Colors.lightBlue,
+    backgroundImage: 'breakfast.jpg',
+  ),
+  Category(
+    id: 'c8',
+    title: 'Asian',
+    color: Colors.lightGreen,
+    backgroundImage: 'asian.jpg',
+  ),
+  Category(
+    id: 'c9',
+    title: 'French',
+    color: Colors.pink,
+    backgroundImage: 'french.jpeg',
+  ),
+  Category(
+    id: 'c10',
+    title: 'Summer',
+    color: Colors.teal,
+    backgroundImage: 'summer.jpg',
+  ),
+];
